72799b482028325eddfc7c51b7f7917e97dea42f
Adam Price <adamprice@fuzzproductions.com>
2012-04-13 20:29:11 -0400

    - Made setting of nab buttons slightly more granular and useful for subclasses. - Changed showRight animation a lot more like what I would expect from this button. - Overall Style Tweaks

---
+++ Example/DDMenuController-Example/DDMenuController-Example/Classes/Controllers/MainNavigationController.h
@@ -0,0 +1,14 @@
+//
+//  MainNavigationController.h
+//  DDMenuController-Example
+//
+//  Created by Adam Price on 4/13/12.
+//  Copyright (c) 2012 Fuzz Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DDMenuController.h"
+
+@interface MainNavigationController : DDMenuController
+
+@end
